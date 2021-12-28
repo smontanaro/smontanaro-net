94f1eefeac38b6db3f9b43b66ab529c7fbdd6250
Skip Montanaro <skip.montanaro@gmail.com>
2021-12-28 14:14:58 +0000

    run from ~/website & enable debugger for now

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+cd ~/website
+
 source ~/website/bin/activate
 
 export FLASK_APP=hello
 export FLASK_ENV=development
 
-flask run --host=0.0.0.0 --port=8080
+flask run --host=0.0.0.0 --port=8080 --debugger
