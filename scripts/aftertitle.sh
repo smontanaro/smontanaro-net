318c1a94552abc29574673d7a9f38e5d40906311
Skip Montanaro <skip.montanaro@gmail.com>
2021-12-29 23:59:47 +0000

    handle any number of files on command line

--- scripts/aftertitle.sh
+++ scripts/aftertitle.sh
@@ -1,10 +1,11 @@
 #/bin/bash
 
-file=$1
+for file in "$@" ; do
+    echo $file
+    if [ ! -r ${file}.orig ] ; then
+        cp -p ${file} ${file}.orig
+    fi
 
-if [ ! -r ${file}.orig ] ; then
-    cp -p ${file} ${file}.orig
-fi
-
-sed -e '/^<title>.*/a <meta http-equiv="content-type" content="text/html; charset="utf-8" />\n<link rel="stylesheet" type="text/css" href="/static/css/default.css" />' < ${file} > ${file}.new
-mv ${file}.new ${file}
+    sed -e '/^<title>.*/a <meta http-equiv="content-type" content="text/html; charset="utf-8" />\n<link rel="stylesheet" type="text/css" href="/static/css/default.css" />' < ${file} > ${file}.new
+    mv ${file}.new ${file}
+done
