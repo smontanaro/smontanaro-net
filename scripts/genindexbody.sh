b3a1330f906a0c4bb69ca63397147b37919b3d6b
Skip Montanaro <skip.montanaro@gmail.com>
2022-01-03 15:58:15 +0000

    simplify

--- scripts/genindexbody.sh
+++ scripts/genindexbody.sh
@@ -4,16 +4,13 @@ cd CR
 
 echo "<ul>"
 
-for f in 20??-??/html/threads.html ; do
-    yr_mo=$(dirname $(dirname $f))
+for yr_mo in 20??-?? ; do
     yr=$(echo $yr_mo | awk -F- '{print $1}')
     mo=$(echo $yr_mo | awk -F- '{print $2}')
     echo "  <li>"
     echo "    ${yr}/${mo}:"
-    for pair in threads.html/threads maillist.html/dates ; do
-        f=$(dirname $pair)
-        txt=$(basename $pair)
-        echo "      <a href='${yr}/${mo}/${f}'>${txt}</a>"
+    for f in threads dates ; do
+        echo "      <a href='${yr}/${mo}/${f}'>${f}</a>"
     done
     echo "  </li>"
 done
