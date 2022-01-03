2326b77ce80662682090790d1d88fb9f33f5c9dc
Skip Montanaro <skip.montanaro@gmail.com>
2022-01-03 15:56:53 +0000

    run from port 80 if run by root

--- run.sh
+++ run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-cd ~/website
+if [ $(id --user) -eq 0 ] ; then
+    port=80
+else
+    port=8080
+fi
 
-source ~/website/bin/activate
+cd ~skip/website
+
+source ~skip/website/bin/activate
 
 export FLASK_APP=hello
 export FLASK_ENV=development
 
-flask run --host=0.0.0.0 --port=8080 --debugger
+flask run --host=0.0.0.0 --port=$port --debugger
