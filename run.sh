0c5a20a0550da5362d8db1e75fc97686431b37f5
Skip Montanaro <skip.montanaro@gmail.com>
2022-01-06 23:58:10 +0000

    push binding into config file

--- run.sh
+++ run.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 if [ $USER = "root" ] ; then
-    port=443
     basedir=/var/opt/website
 
     # refresh venv & website code
     cd /var/opt
     rsync -avs --exclude=CR --exclude='.git*' ~skip/website .
 else
-    port=8080
     basedir=/home/skip/website
 fi
 
@@ -19,4 +17,4 @@ source $basedir/bin/activate
 export FLASK_APP=hello
 export FLASK_ENV=development
 
-gunicorn -c ./gcfg.py -b 0.0.0.0:$port hello:app
+gunicorn -c ./gcfg.py hello:app
