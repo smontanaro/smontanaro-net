82c4230ae42bf22739bbefe4b5f229989cc93877
Skip Montanaro <skip.montanaro@gmail.com>
2022-01-10 22:23:36 +0000

    only runs as me now

--- run.sh
+++ run.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-if [ $USER = "root" ] ; then
-    basedir=/var/opt/website
-
-    # refresh venv & website code
-    cd /var/opt
-    rsync -avs --exclude=CR --exclude='.git*' --exclude=__pycache__ \
-          ~skip/website .
-else
-    basedir=/home/skip/website
-fi
+basedir=/home/skip/website
 
 cd $basedir
 
@@ -18,8 +9,4 @@ source $basedir/bin/activate
 export FLASK_APP=hello
 export FLASK_ENV=development
 
-if [ $USER = "root" ] ; then
-    gunicorn -c ./gcfg.py hello:app
-else
-    flask run -h 0.0.0.0 -p 8080
-fi
+flask run -h 0.0.0.0 -p 8080
