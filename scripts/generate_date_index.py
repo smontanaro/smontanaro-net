4186e14107f2147fc4e831452e3b81c9abd2990e
Skip Montanaro <skip.montanaro@gmail.com>
2022-01-11 18:32:31 -0600

    try two columns

--- scripts/generate_date_index.py
+++ scripts/generate_date_index.py
@@ -29,8 +29,7 @@ def generate_index(records):
     "html fragment output"
     for (dt, chunk) in itertools.groupby(records, date_key):
         print(f'''<h2>{dt.strftime("%d %b %Y")}</h2>''')
-        # print(f'''<a href="#top">Top</a>''')
-        print(f'''<ul class="no-bullets">''')
+        print(f'''<ul style="column-count: 2" class="no-bullets">''')
         for r in chunk:
             print(f'''<li>''')
             print(generate_link(r))
