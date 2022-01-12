9aee72b984125802d982ec55aaa4aa6bdd82a376
Skip Montanaro <skip.montanaro@gmail.com>
2022-01-11 18:33:19 -0600

    four columns, highlight january

--- scripts/genindexbody.sh
+++ scripts/genindexbody.sh
@@ -2,13 +2,17 @@
 
 cd CR
 
-echo '<ul class="no-bullets">'
+echo '<ul style="column-count: 4" class="no-bullets">'
 
 for yr_mo in 20??-?? ; do
     yr=$(echo $yr_mo | awk -F- '{print $1}')
     mo=$(echo $yr_mo | awk -F- '{print $2}')
     echo "  <li>"
-    echo "    ${yr}/${mo}:"
+    if [ $mo = "01" ] ; then
+        echo "    <b>${yr}/${mo}</b>:"
+    else
+        echo "    ${yr}/${mo}:"
+    fi
     for f in threads dates ; do
         echo "      <a href='${yr}/${mo}/${f}'>${f}</a>"
     done
