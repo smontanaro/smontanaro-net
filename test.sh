aa7041a0a1115371ad641c7a129a4f64971f8fbf
Skip Montanaro <skip.montanaro@gmail.com>
2022-01-21 10:00:34 -0600

    in addition to our fixed set of urls, grab some random ones
    as well

--- test.sh
+++ test.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# run Flask server and throw a bunch of URLs at it. Compare with
+# Run Flask server and throw a bunch of URLs at it. Compare with
 # expected output.
+
+# We diddle with localhost.exp toward the end. Make sure it's in
+# a pristine state when we start.
+git restore localhost.exp
 
 export PORT=5001 HOST=localhost
 (bash run.sh 2>&1 \
@@ -12,6 +16,38 @@ sleep 3
 
 sed -e 's/localhost:[0-9][0-9]*/localhost:5001/' < localhost.urls \
     | while read url ; do
+    echo "*** $url ***"
     curl -s $url
 done  2> localhost.act > localhost.raw
+
+# grab a few random pages
+
+for d in $(find CR/ -name '20??-??' \
+               | shuffle \
+               | head -13) ; do
+    find $d -name '*.eml' \
+        | shuffle \
+        | head -3
+done \
+    | sed -e 's:/eml-files/classicrendezvous.[0-9]*.:/:' \
+          -e 's/[.]eml//' \
+    | tr -- '-' / \
+    | while read uri ; do
+    url="http://${HOST}:${PORT}/${uri}"
+    echo "*** $url ***"
+    curl -s $url
+    # avoid spurious diffs
+    echo '127.0.0.1 - - "GET /'${uri}' HTTP/1.1" 200 -' >> localhost.exp
+done >> localhost.raw
+
+# More potentially deceptive diffs. Save these warnings, realizing we
+# expect them on occasion.
+egrep 'WARNING in views: failed to locate' localhost.act > localhost.msgids
+egrep -v 'WARNING in views: failed to locate' localhost.act > localhost.tmp
+mv localhost.tmp localhost.act
+
+if [ -s localhost.msgids ] ; then
+    echo "Note warnings in localhost.msgids"
+fi
+
 pkill -f flask
