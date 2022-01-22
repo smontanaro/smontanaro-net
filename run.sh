a49cc5ddb86e9484226faf58b178b9a4a0bc8542
Skip Montanaro <skip.montanaro@gmail.com>
2022-01-22 17:33:37 -0600

    explicit rc file reference

--- run.sh
+++ run.sh
@@ -3,15 +3,15 @@
 export CRDIR=/home/skip/src/smontanaro.net
 export FLASK_APP=smontanaro:create_app
 export FLASK_ENV=development
-
 PORT=${PORT:-8080}
 HOST=${HOST:-localhost}
 
+RCFILE=$PWD/.coveragerc
 cd smontanaro
 
 CMD="$(which flask) run -h $HOST -p $PORT"
 if [ "x$DOCOVER" = "xtrue" ] ; then
-    coverage run $CMD
+    coverage run -a --rcfile=$RCFILE  $CMD --no-reload
 else
     $CMD
 fi
