0251c771a5981f000c0bfcf948dfef5acab55e4e
Skip Montanaro <skip.montanaro@gmail.com>
2022-02-04 07:42:39 -0600

    disable CSRF for testing, add a couple simple tests

--- smontanaro/tests/test_app.py
+++ smontanaro/tests/test_app.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import tempfile
 
 import pytest
@@ -10,7 +11,12 @@ from smontanaro.db import ensure_db
 @pytest.fixture
 def client():
     db_fd, db_path = tempfile.mkstemp()
-    app = create_app({'TESTING': True, 'REFDB': db_path})
+    app = create_app(
+        {'TESTING': True,
+         'REFDB': db_path,
+         'WTF_CSRF_ENABLED': False,
+         }
+    )
 
     with app.test_client() as client:
         with app.app_context():
@@ -19,3 +25,14 @@ def client():
 
     os.close(db_fd)
     os.unlink(db_path)
+
+def test_get_robots(client):
+    rv = client.get("/robots.txt")
+    assert rv.status_code == 302
+
+def test_post_search(client):
+    rv = client.post("/search", data={
+        "query": "Faliero",
+        "engine": "Brave",
+        })
+    assert rv.status_code == 302
