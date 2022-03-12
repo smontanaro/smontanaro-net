5ea5e0a98841081e45b67441733443ed96c416a9
Skip Montanaro <skip.montanaro@gmail.com>
2022-03-12 07:52:27 -0600

    one line per month to facilitate cheapo filtering

--- scripts/genindexbody.sh
+++ scripts/genindexbody.sh
@@ -7,16 +7,17 @@ echo '<ul style="column-count: auto; column-width: 250px" class="no-bullets">'
 for yr_mo in 20??-?? ; do
     yr=$(echo $yr_mo | awk -F- '{print $1}')
     mo=$(echo $yr_mo | awk -F- '{print $2}')
-    echo "  <li>"
+    echo -n "  <li> "
     if [ $mo = "01" ] ; then
-        echo "    <b>${yr}/${mo}</b>:"
+        echo -n "<b>${yr}-${mo}</b>:"
     else
-        echo "    ${yr}/${mo}:"
+        echo -n "${yr}-${mo}:"
     fi
     for f in threads dates ; do
-        echo "      <a href='${yr}/${mo}/${f}'>${f}</a>"
+        echo -n " <a href='${yr}/${mo}/${f}'>${f}</a>"
     done
-    echo "  </li>"
+    echo -n "  </li>"
+    echo
 done
 
 echo "</ul>"
