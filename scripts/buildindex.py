bad0de7e477feba99b49e094382075521715949b
Skip Montanaro <skip.montanaro@gmail.com>
2022-12-21 21:08:59 -0600

    textblob usage, better post-processing of keys

--- scripts/buildindex.py
+++ scripts/buildindex.py
@@ -10,7 +10,10 @@ import re
 import string
 import sys
 
-from smontanaro.util import read_message, trim_subject_prefix
+from smontanaro.util import read_message, trim_subject_prefix, open_
+from smontanaro.strip import strip_footers, strip_leading_quotes, CRLF
+
+from textblob import TextBlob
 
 
 def main():
@@ -21,16 +24,20 @@ def main():
             return 0
 
     word_map = {}
+    last = ""
     for f in sys.stdin:
+        parts = f.split("/")
+        if parts[1] != last:
+            print(">>>", parts[1], file=sys.stderr)
+            last = parts[1]
         process_file(f.strip(), word_map)
 
     postprocess_map(word_map)
 
-    print("found", len(word_map), "words and phrases")
     if args:
         outfile = args[0]
-        print(f"save map to {outfile}")
-        with open(outfile, "wb") as pobj:
+        print(f"save {len(word_map)} phrases to {outfile}")
+        with open_(outfile, "wb") as pobj:
             pickle.dump(word_map, pobj)
 
     return 0
@@ -40,30 +47,41 @@ def process_file(f, word_map):
     "extract bits from one file"
     f = f.strip()
     msg = read_message(f)
+    # Takes care of the content-transfer-encoding but returns bytes
     payload = msg.get_payload(decode=True)
     if payload is None:
         return
     charset = msg.get_content_charset() or "utf-8"
-    # heuristic: sort so we try latin-1 before utf-8
-    for cs in sorted(set((charset, "utf-8", "latin-1"))):
+    ## print(">>>", f, charset)
+    # Try declared charset (or ascii, if missing), then latin-1 and utf-8 if
+    # necessary.
+    exc = None                            # keep pylint happy
+    for cs in [charset or "ascii"] + ["iso-8859-1", "utf-8"]:
         try:
             payload = payload.decode(cs)
+        # pylint: disable=unused-variable
         except (LookupError, UnicodeDecodeError) as exc:
-            print(f, exc, file=sys.stderr)
-            return
+            continue
         else:
             break
     else:
+        print(f, exc, file=sys.stderr)
         return
-    for word in get_terms(payload):
+
+    quoted = re.compile(r'''\s*"(.*)"\s*$''')
+    subject = trim_subject_prefix(msg["subject"])
+    mat = quoted.match(subject)
+    if mat is not None:
+        # print(f"  {subject} -> {mat.group(1)}")
+        subject = mat.group(1)
+    if subject:
+        payload = f"{subject}{CRLF}{CRLF}{payload}"
+    for word in get_terms(strip_leading_quotes(strip_footers(payload))):
         if word not in word_map:
             word_map[word] = set()
+            if len(word_map) % 1000 == 0:
+                print("...", len(word_map))
         word_map[word].add(f)
-    subject = trim_subject_prefix(msg["subject"])
-    if subject:
-        if subject not in word_map:
-            word_map[subject] = set()
-        word_map[subject].add(f)
 
 
 def read_words(word_file):
@@ -118,7 +136,9 @@ def preprocess(phrase):
         del words[0]
     while words and words[-1] in COMMON_WORDS:
         del words[-1]
-    phrase = " ".join(words)
+    # Somehow we get stuff like "mother 's" out of the noun phrases. Deal with
+    # that... ¯\_(ツ)_/¯
+    phrase = re.sub(" 's( |$)", r"'s\1", " ".join(words))
     return phrase if len(phrase) >= 5 else ""
 
 
@@ -129,7 +149,7 @@ def merge_plurals(k, word_map):
     # "harry hetchins" because "hetchin" isn't in the large dictionary.
     last = k.split()[-1]
 
-    old = new = None
+    old = new = k
     for end in ("s", "es"):
         n = len(end)
         # require truncated last word to be in large dictionary and
@@ -138,7 +158,6 @@ def merge_plurals(k, word_map):
         if (last[:-n] in ALL_WORDS and
             last[-n:] == end and
             k[:-n] in word_map):
-            old = k
             new = k[:-n]
             break
     return (old, new, "plural")
@@ -148,20 +167,17 @@ def merge_ing(k, word_map):
     "map 'ing' endings to base word"
     last = k.split()[-1]
 
-    old = new = None
+    old = new = k
     why = "unknown"
-    for end in ("ing",):
-        n = len(end)
-        if last[-n:] == end:
-            # we would merge "bicycling" into "bicycle" in preference
-            # to "bicycl"
-            for suffix in ("e", ""):
-                if (last[:-n] + suffix in ALL_WORDS and
-                    k[:-n] + suffix in word_map):
-                    old = k
-                    new = k[:-n] + suffix
-                    why = f"-{end}"
-                    break
+    if last[-3:] == "ing":
+        # we would merge "bicycling" into "bicycle" in preference
+        # to "bicycl"
+        for suffix in ("e", ""):
+            if (last[:-3].strip() + suffix in ALL_WORDS and
+                old[:-3].strip() + suffix in word_map):
+                new = old[:-3].strip() + suffix
+                why = "-ing"
+                break
     return (old, new, why)
 
 
@@ -169,11 +185,10 @@ def merge_wrong(k, word_map):
     "merge simple truncations"
     last = k.split()[-1]
 
-    old = new = None
+    old = new = k
     if last not in ALL_WORDS:
         for suffix in ("e", "es", "s"):
             if last + suffix in ALL_WORDS and k + suffix in word_map:
-                old = k
                 new = k + suffix
                 break
     return old, new, "wrong"
@@ -185,14 +200,12 @@ def merge_exceptions(k, word_map):
     # in two ways, either an exact match for `k` or as an exact match for the
     # last word in `k`.
     last = k.split()[-1]
-    old = new = None
+    old = new = k
     why = "noop"
     if k in EXCEPTIONS:
-        old = k
         new = EXCEPTIONS[k]
     elif last in EXCEPTIONS:
-        old = k
-        new = " ".join(k.split()[:-1] + [EXCEPTIONS[last]])
+        new = (" ".join(k.split()[:-1] + [EXCEPTIONS[last]])).strip()
     if new is not None:
         if new not in word_map:
             word_map[new] = set()
@@ -200,27 +213,73 @@ def merge_exceptions(k, word_map):
     return old, new, why
 
 
+def merge_whitespace(k, word_map):
+    "strip trailing whitespace and merge if possible"
+    old = new = k
+    why = "noop"
+    ks = old.strip()
+    if old != ks:
+        new = ks
+        why = "white"
+        if new not in word_map:
+            word_map[new] = set()
+    return old, new, why
+
+
+def strip_nonprint(k, word_map):
+    "strip non-printable characters"
+    old = new = k
+    why = "noop"
+    ks = re.sub(r"[^ -~]+", "", old)
+    if old != ks:
+        new = ks
+        why = "non-print"
+        if new not in word_map:
+            word_map[new] = set()
+    return old, new, why
+
+
+def strip_punct(k, word_map):
+    "strip leading or trailing punctuation or whitespace"
+    old = new = k
+    why = "noop"
+    punct = string.punctuation.replace("-", "")
+    ks = re.sub(f"[ {punct}-]+$", "", old)
+    ks = re.sub(f"^[ {punct}-]+", "", ks).strip()
+    if old != ks:
+        new = ks
+        why = "punct"
+        if new not in word_map:
+            word_map[new] = set()
+    return old, new, why
+
+
 def postprocess_map(word_map):
     "final messing around"
     to_delete = set()
+
     for k in sorted(word_map):
-        if len(k) < 4:
+        if len(k) < 4 or len(k) > 32 or len(word_map[k]) <= 1:
             to_delete.add(k)
             continue
 
-        old = new = None
+        old = new = k
+        why = "noop"
         for merge in (merge_exceptions, merge_plurals, merge_ing,
-                      merge_wrong):
-            old, new, why = merge(k, word_map)
-            if old is not None:
-                break
-        else:
-            continue
+                      merge_wrong, strip_nonprint, strip_punct):
+            # pylint: disable=unused-variable
+            old, new, why = merge(old, word_map)
+            if new != old:
+                print(f"pp: {new} |= {old} ({why})", file=sys.stderr)
+                word_map[new] |= word_map[old]
+                to_delete.add(old)
 
-        if new != old:
-            print(f"pp: {new} |= {old} ({why})", file=sys.stderr)
-            word_map[new] |= word_map[old]
-            to_delete.add(old)
+    if "" in word_map:
+        to_delete.add("")
+
+    for k in word_map:
+        if "bikelist.org" in k:
+            to_delete.add(k)
 
     for k in to_delete:
         del word_map[k]
@@ -234,13 +293,17 @@ EXCEPTIONS = read_csv(os.path.join(os.path.dirname(__file__),
 
 def get_terms(text):
     "yield a series of words matching desired pattern"
-    pat = re.compile(r"([A-Z][-/a-z0-9]+(?: +[A-Z][-/a-z0-9]+)*)")
-    seen = set()
-    for phrase in pat.findall(text):
-        phrase = preprocess(phrase.lower())
-        if not phrase:
+    pat = re.compile(r"[A-Za-z][-/a-z0-9]{2,}"
+                     r"(?:\s+[A-Za-z][-'/a-z0-9]+)*")
+    blob = TextBlob(text)
+    for phrase in sorted(set(blob.noun_phrases)):
+        if pat.match(phrase) is None:
+            # print("   nm:", repr(phrase))
             continue
-        if phrase in seen or len(phrase) < 5:
+        phrase = preprocess(phrase.lower())
+        if (not phrase or
+            " " not in phrase and len(phrase) < 4 or
+            len(phrase) < 7):
             continue
         words = phrase.split()
         while words:
