5ffef4189bf8cdd57a3ddf6d20937d81dd6468d8
Skip Montanaro <skip.montanaro@gmail.com>
2023-01-03 11:35:08 -0600

    expand output to make review easier

--- scripts/csv2topic.py
+++ scripts/csv2topic.py
@@ -8,15 +8,32 @@ for topics table. CSV file is read from stdin. SQL statements go to stdout.
 import csv
 import sys
 
-TEMPLATE = """INSERT INTO topics VALUES('{topic}','{msgid}');"""
+from smontanaro.util import get_message_bits, get_topic
+from smontanaro.refdb import ensure_db
+
+TEMPLATE = """INSERT INTO topics VALUES('{topic}','{message-id}'); -- {topic!r}, {ip}"""
+URL = """https://www.smontanaro.net/{yr:4d}/{mo:02d}/{seq:04d}"""
 
 def main():
     "see __doc__"
+    conn = ensure_db(sys.argv[1])
     rdr = csv.DictReader(sys.stdin)
+    yms = "yr mo seq".split()
     for row in rdr:
-        msgid = row["message-id"]
         for topic in row["topic"].split(","):
-            print(TEMPLATE.format(topic=topic.strip(), msgid=msgid))
+            existing = set([(yr, mo, seq) for (yr, mo, seq, *rest) in get_topic(row["topic"], conn)])
+            if existing:
+                print("--", existing)
+            bits = get_message_bits(row["message-id"], conn)
+            for part in bits:
+                row.update(dict(zip(yms, part)))
+                print("--", URL.format(**row), end=" ")
+                if part in existing:
+                    print("(KNOWN)", end="")
+                print()
+            if not bits:
+                print("--", end=" ")
+            print(TEMPLATE.format(**row))
 
 if __name__ == "__main__":
     sys.exit(main())
