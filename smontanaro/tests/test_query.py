9d94eb55d3cb19674c87c7c2914d4d1f7c18a770
Skip Montanaro <skip.montanaro@gmail.com>
2023-02-02 09:14:58 -0600

    more tests, adapt to new SearchResult

--- smontanaro/tests/test_query.py
+++ smontanaro/tests/test_query.py
@@ -2,7 +2,7 @@
 
 import time
 
-from smontanaro.query import execute_query, parse_query
+from smontanaro.query import execute_query, parse_query, execute_structured_query
 from smontanaro.srchdb import SRCHDB
 from smontanaro.views import query_index
 
@@ -24,27 +24,51 @@ def test_complex_query1(client):
         # CR/2000/10/0885 (bartali and coppi)
         # CR/2000/10/0169 (coppi only)
         # CR/2001/12/0582 (bartali only)
-        filenames = set(res[0] for res in execute_query("bartali OR coppi"))
+        filenames = execute_query("bartali OR coppi").pages()
         assert "CR/2000-10/eml-files/classicrendezvous.10010.0885.eml" in filenames
         assert "CR/2000-10/eml-files/classicrendezvous.10010.0169.eml" in filenames
         assert "CR/2001-12/eml-files/classicrendezvous.10112.0582.eml" in filenames
-        q1 = set(fname for (fname, frag) in execute_query("bartali OR coppi"))
-        q2 = set(fname for (fname, frag) in execute_query("coppi OR bartali"))
-        assert q1 == q2
+
+def test_transitive_complex_query(client):
+    with client.application.app_context():
+        assert (execute_query("coppi AND bartali").pages() ==
+                execute_query("bartali AND coppi").pages())
+
+def test_invalid_not_and_query(client):
+    with client.application.app_context():
+        try:
+            execute_query("NOT coppi AND NOT bartali")
+        except ValueError:
+            pass
+        else:
+            raise ValueError("Failed to catch NOT AND NOT")
+
+def test_invalid_not_or_query(client):
+    with client.application.app_context():
+        try:
+            execute_query("coppi OR NOT bartali")
+        except ValueError:
+            pass
+        else:
+            raise ValueError("Failed to catch OR NOT")
 
 def test_complex_query2(client):
     with client.application.app_context():
         # CR/2000/10/0885 (bartali and coppi)
         # CR/2000/10/0169 (coppi only)
         # CR/2001/12/0582 (bartali only)
-        filenames = set(res[0] for res in execute_query("bartali AND coppi"))
-        assert "CR/2000-10/eml-files/classicrendezvous.10010.0885.eml" in filenames
-        assert "CR/2000-10/eml-files/classicrendezvous.10010.0169.eml" not in filenames
-        assert "CR/2001-12/eml-files/classicrendezvous.10112.0582.eml" not in filenames
-        q1 = set(fname for (fname, frag) in execute_query("bartali AND coppi"))
-        q2 = set(fname for (fname, frag) in execute_query("coppi AND bartali"))
-        assert q1 == q2
+        result = execute_query("bartali AND coppi")
+        assert "CR/2000-10/eml-files/classicrendezvous.10010.0885.eml" in result
+        assert "CR/2000-10/eml-files/classicrendezvous.10010.0169.eml" not in result
+        assert "CR/2001-12/eml-files/classicrendezvous.10112.0582.eml" not in result
 
+def test_complex_query3(client):
+    with client.application.app_context():
+        # CR/2000/10/0885 (bartali and coppi)
+        # CR/2001/12/0582 (bartali only)
+        result = execute_query("bartali AND NOT coppi")
+        assert "CR/2000-10/eml-files/classicrendezvous.10010.0885.eml" not in result
+        assert "CR/2001-12/eml-files/classicrendezvous.10112.0582.eml" in result
 
 def test_query_cache(client):
     # hopefully none of these will already be cached.
@@ -53,13 +77,12 @@ def test_query_cache(client):
         "from:brianbaylis@juno.com",
         "126mm",
         "80s bikes",
-        "ambrosio stem",
-        "ebay problems",
-        "kof rivendell",
-        "madonna del ghisallo",
         "mid-range components",
-        "nervex masi",
         "fausto coppi",
+        "bartali AND coppi",
+        "NOT bartali AND coppi",
+        "from:dale brown AND (silk hope OR McClean)",
+        "from:brian baylist AND richard moon",
         ]
 
     result1 = [0.0, {}]
@@ -68,7 +91,7 @@ def test_query_cache(client):
         for result in (result1, result2):
             start = time.time()
             for query in queries:
-                result[1][query] = list(SRCHDB.get_page_fragments(query))
+                result[1][query] = execute_query(query)
             result[0] = time.time() - start
     # Caching should return the same results ...
     assert result1[1] == result2[1]
@@ -78,18 +101,18 @@ def test_query_cache(client):
 
 def test_from_query(client):
     with client.application.app_context():
-        for (filename, fragment) in SRCHDB.get_page_fragments("from:mark petry"):
-            assert filename
-            assert not fragment
-            break
-        else:
+        result = execute_query("from:mark petry")
+        try:
+            assert result.data
+        except AssertionError:
             raise ValueError("no search results")
+        assert set(result.data.values()) == set([""])
 
 def test_trailing_space_from(client):
     with client.application.app_context():
-        no_space = list(query_index("from:dale brown"))
-        tr_space = list(query_index("from:dale brown "))
-        assert tr_space and no_space and len(no_space) == len(tr_space)
+        no_space = execute_query("from:dale brown")
+        tr_space = execute_query("from:dale brown ")
+        assert tr_space and tr_space == no_space
 
 
 def test_parse_simple_query():
@@ -104,12 +127,12 @@ def test_parse_or_query():
         ["union", ["search", "valetti"], ["search", "bartali"]]
 
 def test_parse_and_or_query():
-    assert parse_query("Valetti OR Bartali AND Coppi") == \
+    assert parse_query("Valetti OR Bartali AND NOT Coppi") == \
         ["union",
          ["search", "valetti"],
          ["intersect",
           ["search", "bartali"],
-          ["search", "coppi"],
+          ["not", ["search", "coppi"]],
          ]]
 
 def test_parse_parens_query():
@@ -121,3 +144,29 @@ def test_parse_parens_query():
          ],
          ["search", "coppi"],
         ]
+
+def test_parse_not_and_not_query():
+    assert parse_query("(NOT coppi) AND (NOT bartali)") == \
+        ["intersect",
+         ["not", ["search", "coppi"]],
+         ["not", ["search", "bartali"]]]
+
+def test_invalid_parse1():
+    parsed_query = parse_query("bartali")
+    parsed_query[0] = "fred"
+    try:
+        execute_structured_query(parsed_query)
+    except ValueError:
+        pass
+    else:
+        raise ValueError("Failed to reject bad parse")
+
+def test_invalid_parse():
+    parsed_query = parse_query("bartali AND coppi")
+    parsed_query[0] = "fred"
+    try:
+        execute_structured_query(parsed_query)
+    except ValueError:
+        pass
+    else:
+        raise ValueError("Failed to reject bad parse")
