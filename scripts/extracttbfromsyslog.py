7b5893b8d7351a0621a402855d9481bd83846c0c
Skip Montanaro <skip.montanaro@gmail.com>
2023-02-06 20:34:56 -0600

    script to get tracebacks from syslog

---
+++ scripts/extracttbfromsyslog.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+"Extract gunicorn traceback info from syslog"
+
+import sys
+
+import regex as re
+
+def main():
+    tb_line = re.compile(r"gunicorn[[][0-9]+[]]: Traceback")
+    inside_line = re.compile(r"gunicorn[[][0-9]+[]]:  +")
+
+    state = "start"
+    for line in sys.stdin:
+        match state:
+            case "start":
+                if tb_line.search(line) is not None:
+                    state = "inside"
+                    print(line.rstrip())
+            case "inside":
+                if inside_line.search(line) is not None:
+                    print(line.rstrip())
+                else:
+                    state = "counting"
+                    n = 1
+                    print(line.rstrip())
+            case "counting":
+                if tb_line.search(line) is not None:
+                    # Bump into the next traceback
+                    print(line.rstrip())
+                    state = "inside"
+                elif n >= 3:
+                    print()
+                    state = "start"
+                else:
+                    n += 1
+                    print(line.strip())
+
+if __name__ == "__main__":
+    sys.exit(main())
