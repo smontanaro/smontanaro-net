7364dace3ac472528bb0dcfd116111866acb89df
Skip Montanaro <skip.montanaro@gmail.com>
2023-04-04 17:00:06 -0500

    simplify by using read_message()

--- scripts/idwj.py
+++ scripts/idwj.py
@@ -6,19 +6,18 @@ import csv
 import email
 import sys
 
+from smontanaro.util import read_message
+
 def main():
-    writer = csv.DictWriter(sys.stdout, ["sender", "wjref"], quoting=csv.QUOTE_ALL)
+    writer = csv.DictWriter(sys.stdout, ["sender", "wjref"],
+        quoting=csv.QUOTE_ALL)
     writer.writeheader()
     for fname in sys.stdin:
-        with open(fname.strip(), "rb") as fbytes:
-            msg = email.message_from_bytes(fbytes.read())
-            sender = msg["From"].strip()
-            try:
-                for word in msg.get_payload(decode=False).split():
-                    if "wooljersey.com/gallery" in word:
-                        writer.writerow({"sender": sender, "wjref": word})
-            except AttributeError:
-                print(fname, "multipart?", file=sys.stderr)
+        msg = read_message(fname.strip())
+        sender = msg["From"].strip()
+        for word in msg.extract_text().split():
+            if "wooljersey.com" in word:
+                writer.writerow({"sender": sender, "wjref": word})
 
 if __name__ == "__main__":
     sys.exit(main())
