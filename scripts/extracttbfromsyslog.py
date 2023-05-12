1221267367dfe4f14c6724dd0725ce6e8c40d990
Skip Montanaro <skip.montanaro@gmail.com>
2023-05-12 16:36:26 -0500

    no empty output

--- scripts/extracttbfromsyslog.py
+++ scripts/extracttbfromsyslog.py
@@ -10,30 +10,41 @@ def main():
     inside_line = re.compile(r"gunicorn\[[0-9]+\]:  +")
 
     state = "start"
+    nprints = 0
     for line in sys.stdin:
         match state:
             case "start":
                 if tb_line.search(line) is not None:
                     state = "inside"
                     print(line.rstrip())
+                    nprints += 1
             case "inside":
                 if inside_line.search(line) is not None:
                     print(line.rstrip())
+                    nprints += 1
                 else:
                     state = "counting"
                     n = 1
                     print(line.rstrip())
+                    nprints += 1
             case "counting":
                 if tb_line.search(line) is not None:
                     # Bump into the next traceback
                     print(line.rstrip())
+                    nprints += 1
                     state = "inside"
                 elif n >= 3:
                     print()
+                    nprints += 1
                     state = "start"
                 else:
                     n += 1
                     print(line.strip())
+                    nprints += 1
+
+    if nprints == 0:
+        print("No recent tracebacks to study")
+
 
 if __name__ == "__main__":
     sys.exit(main())
