70283219fce2b2e943b397bc8e479caa31d002ce
Skip Montanaro <skip.montanaro@gmail.com>
2023-07-30 21:17:30 -0500

    add --empty flag and relpath() func

--- scripts/crcachectl.py
+++ scripts/crcachectl.py
@@ -17,6 +17,8 @@ def main():
                         help="List cache keys", action="store_true")
     parser.add_argument("-d", "--delete", dest="keys", action="append",
                         help="Delete the given key")
+    parser.add_argument("-e", "--empty", dest="empty", action="store_true",
+                        help="Delete all empty keys")
     parser.add_argument("--delete-all", dest="delete_all", action="store_true",
                         default=False, help="Delete all keys")
     parser.add_argument("--dir", dest="dir", default="./search_cache",
@@ -27,7 +29,7 @@ def main():
         print("Can't be both quiet and verbose!", file=sys.stderr)
         return 1
 
-    with open(os.path.join(args.dir, "index.pkl"), "rb") as cache:
+    with open(relpath("index.pkl", args.dir), "rb") as cache:
         index = pickle.load(cache)
 
     if args.list:
@@ -39,6 +41,13 @@ def main():
                     print(" ", key, end="")
                 if args.verbose:
                     print(f" ({index[key]})", end="")
+                    path = relpath(index[key], args.dir)
+                    if not os.path.exists(path):
+                        print(" missing", end="")
+                    else:
+                        with open(path, "rb") as fp:
+                            if not pickle.load(fp):
+                                print(" empty", end="")
                 if not args.quiet:
                     print()
         else:
@@ -46,29 +55,43 @@ def main():
                 print("Empty cache")
             return 0
 
-    if args.keys is not None or args.delete_all:
-        delfnames = set()
-        dkeys = args.keys if not args.delete_all else set(index)
-        for key in dkeys:
-            if key in index:
-                # Despite abs path in the index, only remove files relative to
-                # the cache dir.  This is marginally safer and makes testing
-                # easier.
-                fname = os.path.join(args.dir, os.path.basename(index[key]))
-                if not args.quiet:
-                    print("delete", key, fname)
-                delfnames.add(fname)
-                try:
-                    os.unlink(fname)
-                except FileNotFoundError:
-                    pass
-                del index[key]
-        if delfnames:
-            # modified, rewrite index file
-            with open(os.path.join(args.dir, "index.pkl"), "wb") as cache:
-                pickle.dump(index, cache)
+    delkeys = set()
+    if args.empty:
+        for key in index:
+            path = relpath(index[key], args.dir)
+            if not os.path.exists(path):
+                delkeys.add(key)
+            else:
+                with open(relpath(index[key], args.dir), "rb") as fp:
+                    if not pickle.load(fp):
+                        delkeys.add(key)
+    elif args.keys is not None:
+        delkeys = set(args.keys)
+    elif args.delete_all:
+        # convert keys to set() to allow deletion in the loop
+        delkeys = set(index)
+    for key in delkeys:
+        # Despite abs path in the index, only remove files relative to
+        # the cache dir.  This is marginally safer and makes testing
+        # easier.
+        path = relpath(index[key], args.dir)
+        if os.path.exists(path):
+            if not args.quiet:
+                print("delete", repr(key), path)
+            try:
+                os.unlink(path)
+            except FileNotFoundError:
+                pass
+        del index[key]
+    if delkeys:
+        with open(relpath("index.pkl", args.dir), "wb") as cache:
+            pickle.dump(index, cache)
 
     return 0
+
+def relpath(path, basedir):
+    """map path to basedir"""
+    return os.path.join(basedir, os.path.basename(path))
 
 if __name__ == "__main__":
     sys.exit(main())
