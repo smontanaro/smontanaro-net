7a92ebf5601a7f56e3588abc94c28f71ac695d73
Skip Montanaro <skip.montanaro@gmail.com>
2025-03-02 06:33:22 -0600

    make sure we have gunicorn

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,6 +12,11 @@ if [ "x$DOCOVER" = "xtrue" ] ; then
     COV="coverage run -a --rcfile=$RCFILE"
 fi
 
+if [ "x$(which gunicorn)" = "x" ] ; then
+    echo "Gunicorn not found. Make sure you're running an appropriate virtual environment." 1>&2
+    exit 1
+fi
+
 ${COV} $(which gunicorn) --pythonpath=$(pwd)/smontanaro \
        --error-logfile=/dev/stderr --access-logfile=/dev/stderr \
        --bind $HOST:$PORT wsgi:app
